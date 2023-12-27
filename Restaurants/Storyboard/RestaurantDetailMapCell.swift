3bd7d213242078f65f564d365c457c18c5a88790
Neatwhiskey <neatwhiskey24@gmail.com>
2023-12-27 23:49:30 +0100

    1 Embedded a mapView in restaurantDetailController 2 Translated the address of the restaurant into a coordinate

---
+++ Restaurants/Storyboard/RestaurantDetailMapCell.swift
@@ -0,0 +1,23 @@
+//
+//  RestaurantDetailMapCell.swift
+//  Restaurants
+//
+//  Created by Jamaaldeen Opasina on 27/12/2023.
+//
+
+import UIKit
+
+class RestaurantDetailMapCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
