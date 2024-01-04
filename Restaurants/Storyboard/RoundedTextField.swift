cfd31587cb5f1df892d369ccc618d107562fdd0b
Neatwhiskey <44437144+Neatwhiskey@users.noreply.github.com>
2024-01-04 22:34:23 +0100

    Creating New Restaurant scene

---
+++ Restaurants/Storyboard/RoundedTextField.swift
@@ -0,0 +1,20 @@
+//
+//  RoundedTextView.swift
+//  Restaurants
+//
+//  Created by Jamaaldeen Opasina on 04/01/2024.
+//
+
+import UIKit
+
+class RoundedTextView: UITextView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
